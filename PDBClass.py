e43437d24d1585625477e5e5d0a253ccab7d2ab6
Quinten Reed <qreed26@cpatech.org>
2025-02-11 17:53:51 +0000

    my notes

---
+++ PDBClass.py
@@ -0,0 +1,95 @@
+# Quinten Reed
+# U5L1
+# SinglyLinkedList
+
+class PositionalDoublyBase():
+  class DoublyNode():
+    def __init__(self, value):
+      self.value = value
+      self.next = None
+      self.prev = None
+    
+    def __str__(self):
+      """Convert individual node to string"""
+      return f"|{self.value}|"
+
+    def set_next(self, value):
+      """Sets the next node in the list"""
+      if type(value) == type(self) or value == None:
+        self.next = value
+      else:
+        raise TypeError("Needs to be a node")
+
+    def set_prev(self, value):
+      """Sets the previous node in the list"""
+      if type(value) == type(self) or value == None:
+        self.prev = value
+      else:
+        raise TypeError("Needs to be a node")
+
+  def __init__(self):
+    self.header = self.DoublyNode(None)
+    self.trailer = self.DoublyNode(None)
+
+    self.header.set_next(self.trailer)
+    self.trailer.set_prev(self.header)
+
+    self.__size = 0
+
+  def __str__(self):
+    """Convert list to string"""
+    out = "HEADER:"
+
+    walk = self.header
+    for i in range(self.__size+2):
+      out += f"{walk}"
+      walk = walk.next
+      if walk != None:
+        out += " <-> "
+
+    out += ":TRAILER"
+    return out
+
+  def __len__(self):
+    """Returns length of linked list"""
+    return self.__size
+
+  def __is_empty(self):
+    """Returns whether the linked list is empty"""
+    return self.__size <= 0
+
+  def __insert_between(self, value, pred = False, succ = False):
+    """Inserts a new node into the list"""
+    if pred == False:
+      pred = self.header
+
+    if succ == False:
+      succ = self.trailer
+
+    if pred == succ.prev:
+      new = self.DoublyNode(value)
+      new.set_next(succ)
+      new.set_prev(pred)
+
+      pred.set_next(new)
+      succ.set_prev(new)
+
+      self.__size += 1
+      return new
+    else:
+      raise IndexError("elements exist between these elements already")
+
+  def __delete_node(self, node):
+    """Deletes a node from the list"""
+    if node.value != None:
+      deleted = node.value
+
+      node.prev.set_next(node.next)
+      node.next.set_prev(node.prev)
+
+      node.value = node.prev = node.next = None
+
+      self.__size -= 1
+      return deleted
+    else:
+      raise ValueError("can't delete node with None value (either sentinel or already deleted)")
---
+++ main.py
@@ -0,0 +1,44 @@
+# Implementation & testing of the PositionalDoublyBase class
+
+from PDBClass import PositionalDoublyBase
+from TEST_CODE import *
+import os
+
+'''
+Testing details can be found in TEST_CODE.py
+
+ENSURE ALL TESTS PASS BEFORE SUBMITTING
+
+IF COLORAMA NOT FOUND - ENTER INTO TERMINAL:
+pip install colorama
+'''
+
+def main():
+
+    PDB = PositionalDoublyBase()
+
+    # TEST 1 - Test PDB Creation
+    # BEFORE TESTING: implement PBD __init__
+    TEST_new_PDB(PDB, PositionalDoublyBase)
+
+    # TEST 2 - Test DoublyNode
+    # BEFORE TESTING: implement DoublyNode class
+    TEST_doubly_node(PositionalDoublyBase)
+
+    # TEST 3 - Test insert_between
+    # BEFORE TESTING: implement PBD insert_between, len, is_empty
+    nodes = TEST_insert_between(PDB, PositionalDoublyBase)
+
+    # TEST 4 - Test delete_node
+    # BEFORE TESTING: implement PBD delete_node
+    TEST_delete_node(PDB, PositionalDoublyBase, nodes)
+
+    # TEST 5 - Test docstrings
+    # BEFORE TESTING: implement method docstrings
+    TEST_docs(PDB, PositionalDoublyBase)
+    
+
+
+if __name__ == "__main__":
+    os.system("clear")
+    main()
